2e4657572c50207846e0e46d3118d506ae8f31fb
henne <accounts@hfellerhoff.de>
2022-03-02 20:41:28 +0100

    feat: disable features

---
+++ configs.go
@@ -0,0 +1,7 @@
+package escpos
+
+var (
+	ConfigEpsonTMT20II = PrinterConfig{}
+	ConfigEpsonTMT88II = PrinterConfig{DisableUpsideDown: true}
+	ConfigSOL802       = PrinterConfig{DisableUpsideDown: true}
+)
